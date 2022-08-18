ba2c37a10315b0c532a2a9c438925dc9b77a1a3e
Timur Azizov <tazizov2001@gmail.com>
2022-08-18 22:10:11 +0500

    settings upd

--- news_project/settings.py
+++ news_project/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news.apps.NewsConfig',
+
     'rest_framework',
+
+    'news',
 ]
 
 REST_FRAMEWORK = {
