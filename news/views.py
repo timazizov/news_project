aa5f7651619616194ee0614244f51802d6bb0dcb
Timur Azizov <tazizov2001@gmail.com>
2022-08-18 22:24:29 +0500

    added pagination

--- news/views.py
+++ news/views.py
@@ -2,11 +2,13 @@ from rest_framework import viewsets
 
 from news.models import News, Category
 from news.serializers import NewsSerializer, CategorySerializer
+from news.utils import NewsPagination
 
 
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+    pagination_class = NewsPagination
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
--- news_project/settings.py
+++ news_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6g_$alrhrsvj00y3!j2&wyj(@s6&xs6+570g+)*)2x=bu9m0g*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
